4662b47266f80efffd7f78067207280eca67d0dc
Oskar Samborowski <94199123+Haz-art@users.noreply.github.com>
2024-06-01 22:46:12 +0200

    new handler

--- System ERP/Form3.Designer.cs
+++ System ERP/Form3.Designer.cs
@@ -28,18 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            SuspendLayout();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 484);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(369, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "SEND";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(74, 93);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(74, 147);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 2;
             // 
             // Form3
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(393, 540);
-            Name = "Form3";
-            Text = "Form3";
-            ResumeLayout(false);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 540);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
     }
 }
--- System ERP/Form3.cs
+++ System ERP/Form3.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,25 @@ namespace System_ERP
         public Form3()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text1 = textBox1.Text;
+            string text2 = textBox2.Text;
+
+            MessageBox.Show(text1[0] + " . " + text2[0] + " Witaj w systemie jako " + text1 + "  " + text2);
+
+
+            string createText = text1 +", "+text2 +  DateTime.Now + Environment.NewLine;
+            try{
+                File.WriteAllText("./", createText);
+            }
+            catch
+            {
+                MessageBox.Show("Nie znaleziono pliku,nie zapisano zmian");
+
+            }
         }
     }
 }
